dac111b0a0e9615ed66a2b00c0c06f8bc215fe91
Lucas Fievet <lucas.fievet@gmail.com>
2014-07-10 18:51:05 +0200

    Latest version.

--- ABM/sim_abs.py
+++ ABM/sim_abs.py
@@ -1,6 +1,6 @@
 __author__ = 'lfi'
 
-from numpy import exp, random
+from numpy import exp, random, arange
 
 from sim_base import SimBase
 
@@ -10,13 +10,14 @@ class SimABS(SimBase):
         'r': 0.001,
         'y_avg': 1.0,
         'ps': 1000,
-        'v': 0.95,
+        'v': 0.9,
         'g': 1.9,
         'a': 1.0,
         'o': 1.0,
         'b': 2.0,
         'n': 0.99,
         'alpha': 1800,
+        'noise': 25.0,
     }
     variables = {
         'n1': [],
@@ -25,12 +26,16 @@ class SimABS(SimBase):
         'U1': [0.0],
         'U2': [0.0],
         'y': [],
+        'epsilon': [],
     }
     updates = [
-        ["y", "n1", "n2"],
+        ["epsilon", "y", "n1", "n2"],
         ["p"],
         ["U1", "U2"],
     ]
+
+    def update_epsilon(self):
+        return random.normal(0, self.noise)
 
     def update_y(self):
         return random.normal(self.y_avg, self.o)
@@ -39,13 +44,13 @@ class SimABS(SimBase):
         return 1 - self.update_n2()
 
     def update_n2(self):
-        nt2 = 1 / (exp(self.b * (self.U1 -self.U2)) + 1)
+        nt2 = 1 / (exp(self.b * (self.U1 - self.U2)) + 1)
         return nt2 * exp(-(self.p - self.ps) * (self.p - self.ps) / self.alpha)
 
     def update_p(self):
         return self.n1 * (self.ps + self.v * (self.p - self.ps)) +\
             self.n2 * (self.p + self.g * (self.p - self.p_2)) +\
-            self.y_avg
+            self.y_avg + self.epsilon
 
     def update_U1(self):
         return (self.p + self.y - (1 + self.r) * self.p_2) *\
@@ -63,7 +68,10 @@ if __name__ == '__main__':
     sim = SimABS()
     sim.print_parameters()
     print sim
-    for i in range(0, 100):
-        sim.run(1)
-        print sim
-    sim.plot("p")
+    sim.scan_parameter("v", arange(0.5, 1.01, 0.01), "std")
+    # sim.run_until("std", 1E-4)
+    # for i in range(0, 100000):
+    #     sim.run(1)
+        # print sim
+    # sim.print_diff_information("p")
+    # sim.plot("p")
--- ABM/sim_base.py
+++ ABM/sim_base.py
@@ -1,7 +1,11 @@
 __author__ = 'lfi'
 
+import copy
 import re
 import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.stats import skew, kurtosis
 
 
 class SimBase(object):
@@ -34,6 +38,53 @@ class SimBase(object):
                     new_values[key] = self.__getattribute__("update_{0}".format(key))()
                 for key in keys:
                     self.__setattr__(key, new_values[key])
+
+    def run_until(self, name, upper=1E-3):
+        old_value, new_value = 0.0, 1.0
+        while abs(new_value - old_value) > upper:
+            self.run(1000)
+            old_value = new_value
+            new_value = self.__getattribute__("diff_{0}".format(name))("p")
+            # print "Change {0}: {1}.".format(name, new_value - old_value)
+        return new_value
+
+    def scan_parameter(self, parameter_name, values, property_name):
+        initial_variables = copy.copy(self.variables)
+        for value in values:
+            self.variables = initial_variables
+            self.__setattr__(parameter_name, value)
+            property_value = self.run_until(property_name)
+            print "{0}={1} => {2}={3}".format(parameter_name, value, property_name, property_value)
+
+    def diff_method(self, name, method):
+        array = np.array(self.variables[name])
+        array_diff = np.diff(array)
+        return method(array_diff)
+
+    def diff_mean(self, name):
+        return self.diff_method(name, np.mean)
+
+    def diff_median(self, name):
+        return self.diff_method(name, np.median)
+
+    def diff_std(self, name):
+        return self.diff_method(name, np.std)
+
+    def diff_skewness(self, name):
+        return self.diff_method(name, skew)
+
+    def diff_kurtosis(self, name):
+        return self.diff_method(name, kurtosis)
+
+    def print_diff_information(self, name):
+        print "----------"
+        print "Variable {0}".format(name)
+        print "----------"
+        print "Mean: {0}".format(self.diff_mean(name))
+        print "Median: {0}".format(self.diff_median(name))
+        print "Standard: {0}".format(self.diff_std(name))
+        print "Skewness: {0}".format(self.diff_skewness(name))
+        print "Kurtosis: {0}".format(self.diff_kurtosis(name))
 
     def plot(self, *args):
         names = args[0:]
